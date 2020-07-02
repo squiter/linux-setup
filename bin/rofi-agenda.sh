636a8305256726b9184433580172f26867173ba4
Brunno dos Santos <brunno@brunno.me>
2020-07-02 17:03:20 -0300

    Add a cache to my calendar function
    Here we have an example of how to use -kb-custom on rofi!

--- bin/rofi-agenda.sh
+++ bin/rofi-agenda.sh
@@ -5,21 +5,34 @@ set -e
 
 function get_events()
 {
-    gcalcli --nocolor agenda
+    with_cache gcalcli --nocolor agenda
+}
+
+clear_cache(){
+    echo "Cleaning the cache!"
+    rm $HOME/.cache/with_cache/gcalcli---nocolor-agenda
+    main
 }
 
 function main()
 {
-    local all_events="$(get_events)"
-    echo $all_events
-    local event=$( (echo "${all_events}")  | rofi -dmenu -i -p "Next events :D")
-
-    # if [[ -z "$site" ]];then
-    #     return 0
-    # else
-    #     local lpass_id=$(echo "$site" | sed -e 's/.*\[id\:\ \(.*\)\]/\1/')
-    #     lpass show --password -c "$lpass_id"
-    # fi
+    if item=$(
+            local all_events="$(get_events)"
+            echo "${all_events}" \
+                | rofi -dmenu \
+                       -i \
+                       -p "Next events :D" \
+                       -kb-custom-1 "Alt+r" \
+                       -mesg "Alt+r to Refresh the content"
+           ); then
+        echo "just to have a true statement!"
+    else
+        rofi_exit_code=$?
+        case $rofi_exit_code in
+            10) clear_cache;;
+            *) exit $?;;
+        esac
+    fi
 }
 
 main
